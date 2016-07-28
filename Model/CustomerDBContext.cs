a367639ed03eb251e2630a772827da82738fa815
dym0 <damiankuzmicki1989@gmail.com>
2016-07-28 18:31:15 +0200

    add entity framework

---
+++ Model/CustomerDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace invoiceCmd.Model
+{
+    class CustomerDBContext : DbContext
+    {
+
+        public CustomerDBContext() : base("name=CustomerContext")
+        {
+
+        }
+        public DbSet<DbCustomer> Customers { get; set; }
+    }
+}
---
+++ Model/CustomerModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace invoiceCmd.Model
+{
+    class DbCustomer : ICustomer
+    {
+        public int ID { get;  set;  }
+
+        public string Name {  get; set;}
+
+        public string Surname { get; set;  }
+
+        public string Email { get;  set;}
+
+        public string Mobile {  get; set; }
+
+        public DateTime Date { get; set;  }
+        
+    }
+}
